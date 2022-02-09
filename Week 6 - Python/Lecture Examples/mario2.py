76c69ab1d080d2cd02fed825aaee4181e0a62d47
AndrewChiaYJ <chiayj95@hotmail.com>
2022-02-09 22:04:20 +0800

    Week 6 Folder

---
+++ Week 6 - Python/Lecture Examples/mario2.py
@@ -0,0 +1,13 @@
+for i in range(4):
+    print("?",end ="")
+print()
+
+print("?" * 4)
+
+for i in range (3):
+    for j in range (3):
+        print("#", end="")
+    print()
+
+for k in range (3):
+    print("#" * 3)
